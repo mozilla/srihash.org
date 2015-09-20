9c048e9d1442a514316cce9fee1135bd227c8663
Francois Marier <francois@mozilla.com>
2015-09-20 15:38:17 -0700

    Update all external dependencies
    The code changes come from the upgrade to hapi 10.x.x.

--- index.js
+++ index.js
@@ -14,75 +14,79 @@ var helpers = require('./lib/helpers.js');
 var server = new Hapi.Server();
 server.connection({ port: 4000 });
 
-server.views({
-  engines: {
-    html: handlebars
-  },
-  path: Path.join(__dirname, 'templates')
+server.register(require('vision'), function (err) {
+  server.views({
+    engines: {
+      html: handlebars
+    },
+    path: Path.join(__dirname, 'templates')
+  });
 });
 
-/**
- * Serve index.js
- */
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply.view('index', { 'title': 'SRI Hash Generator' });
-  }
-});
-
-/**
- * Serve public files
- */
-server.route({
-  method: 'GET',
-  path: '/{param*}',
-  handler: {
-    directory: {
-      path: 'public',
-      lookupCompressed: true
+server.register(require('inert'), function (err) {
+  /**
+   * Serve index.js
+   */
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply.view('index', { 'title': 'SRI Hash Generator' });
     }
-  }
-});
+  });
 
-/**
- * Return SRI lookup in JSON format
- */
-server.route({
-  method: 'POST',
-  path: '/generate',
-  handler: function (request, reply) {
-    var options = {
-      url: request.payload.url,
-      algorithms: request.payload.algorithms
-    };
-    helpers.generate(options, function (result) {
-      reply(
-        JSON // jshint ignore:line
-          .stringify(result)
-      ).type('application/json');
-    });
-  }
-});
-
-
-/**
- * Return SRI lookup in HTML format.
- * Deprecated, pending move to isomorphic app.
- */
-server.route({
-  method: 'POST',
-  path: '/hash',
-  handler: function (request, reply) {
-    helpers.generateElement(
-      request.payload.url,
-      request.payload.algorithms,
-      function (result) {
-        reply.view('hash', { 'hash': result });
+  /**
+   * Serve public files
+   */
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: 'public',
+        lookupCompressed: true
       }
-    );
-  }
+    }
+  });
+
+  /**
+   * Return SRI lookup in JSON format
+   */
+  server.route({
+    method: 'POST',
+    path: '/generate',
+    handler: function (request, reply) {
+      var options = {
+        url: request.payload.url,
+        algorithms: request.payload.algorithms
+      };
+      helpers.generate(options, function (result) {
+        reply(
+          JSON // jshint ignore:line
+            .stringify(result)
+        ).type('application/json');
+      });
+    }
+  });
+
+
+  /**
+   * Return SRI lookup in HTML format.
+   * Deprecated, pending move to isomorphic app.
+   */
+  server.route({
+    method: 'POST',
+    path: '/hash',
+    handler: function (request, reply) {
+      helpers.generateElement(
+        request.payload.url,
+        request.payload.algorithms,
+        function (result) {
+          reply.view('hash', { 'hash': result });
+        }
+      );
+    }
+  });
 });
 
 server.start(function () {
