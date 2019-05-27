ebf43e6d410e988811308a544ebe8e22a6213411
Johann-S <johann.servoire@gmail.com>
2019-05-27 11:49:55 +0200

    fix multi redirects and add a unit test

--- test/generate.js
+++ test/generate.js
@@ -53,6 +53,36 @@ describe('generate()', () => {
     assert.deepEqual(result.integrity, resultWithoutRedirect.integrity);
   });
 
+  it('multi redirect', async() => {
+    /**
+     * https://bit.ly/2X6ThH3 is redirected to https://gitcdn.xyz/repo/mozilla/srihash.org/master/index.js
+     * which is redirect to https://gitcdn.xyz/cdn/mozilla/srihash.org/e6a30b7403defe47af01a86e03a83c11b8a73786/index.js
+     */
+    const resource = {
+      url: 'https://bit.ly/2X6ThH3',
+      algorithms: ['sha384']
+    };
+
+    const resourceWithoutRedirect = {
+      url: 'https://gitcdn.xyz/cdn/mozilla/srihash.org/e6a30b7403defe47af01a86e03a83c11b8a73786/index.js',
+      algorithms: ['sha384']
+    };
+
+    const expect = {
+      success: true,
+      status: 200,
+      url: 'https://gitcdn.xyz/cdn/mozilla/srihash.org/e6a30b7403defe47af01a86e03a83c11b8a73786/index.js',
+      type: 'js',
+      integrity: 'sha384-6ZfLLCEAJcSxt9bD8PaRKJhpgbSPZj9GUFuGSikIgVaBpaQWpkXkwyu63GHVA3sp',
+      eligibility: []
+    };
+    const result = await generate(resource);
+    const resultWithoutRedirect = await generate(resourceWithoutRedirect);
+
+    assert.deepEqual(result, expect);
+    assert.deepEqual(result.integrity, resultWithoutRedirect.integrity);
+  });
+
   it('css', async() => {
     const resource = {
       url: 'https://code.jquery.com/ui/1.11.3/themes/black-tie/jquery-ui.css',
