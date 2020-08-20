11d91a32d6d305a0dd681e21861762fecb74dfe3
Kota Yatagai <kota@yatagai.com>
2020-08-20 12:52:28 +0000

    Refer to https://github.com/github/clipboard-copy-element/blob/main/src/clipboard.ts

--- app.js
+++ app.js
@@ -37,6 +37,59 @@ async function hashText(message) {
   return hash384;
 }
 
+function createNode(text) {
+  const node = document.createElement('pre');
+
+  node.style.width = '1px';
+  node.style.height = '1px';
+  node.style.position = 'fixed';
+  node.style.top = '5px';
+  node.textContent = text;
+  return node;
+}
+
+function copyNode(node) {
+  if ('clipboard' in navigator) {
+    return navigator.clipboard.writeText(node.textContent || '');
+  }
+
+  const selection = getSelection();
+
+  if (selection === null) {
+    return Promise.reject(new Error());
+  }
+
+  selection.removeAllRanges();
+
+  const range = document.createRange();
+
+  range.selectNodeContents(node);
+  selection.addRange(range);
+
+  document.execCommand('copy');
+  selection.removeAllRanges();
+  return Promise.resolve();
+}
+
+function copyText(text) {
+  if ('clipboard' in navigator) {
+    return navigator.clipboard.writeText(text);
+  }
+
+  const { body } = document;
+
+  if (!body) {
+    return Promise.reject(new Error());
+  }
+
+  const node = createNode(text);
+
+  body.appendChild(node);
+  copyNode(node);
+  body.removeChild(node);
+  return Promise.resolve();
+}
+
 async function formSubmit(event) {
   event.preventDefault();
   resetInterface();
@@ -68,8 +121,7 @@ async function formSubmit(event) {
       const sriCopy = document.getElementById("sriCopy");
 
       sriCopy.addEventListener("click", () => {
-      // eslint-disable-next-line no-new
-        new ClipboardJS("#sriCopy"); // eslint-disable-line no-undef
+        copyText(scriptEl);
       });
     } else {
       console.error("Non-OK HTTP response status. Error.");
